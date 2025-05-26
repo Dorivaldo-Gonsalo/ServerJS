9751ab3a8e8f8b768177b2dcacd7874f33a144b2
Dorivaldo-Gonsalo <dorivaldogoncaloferreira011@gmail.com>
2025-05-26 13:53:50 +0100

    Separação dos links

--- routes/master.js
+++ routes/master.js
@@ -3,7 +3,7 @@ const router = Router();
 
 router.get('/', (req, res)=>{
     res.send(
-        "<h1 style='text-align:center;margin:10% 20%'> Bem-Vindo ao Sistema de gerenciamento da Relaxe.ao (SGR) </h1><br> <button>Usuários</button> <button>Contratos</button><button>Residências</button><button>Master</button>"
+        "<h1 style='text-align:center;margin:10% 20%'> Bem-Vindo ao Sistema de gerenciamento da Relaxe.ao (SGR) </h1><br> <button>Usuários</button> <button>Contratos</button> <button>Residências</button> <button>Master</button>"
     );
     
 });
