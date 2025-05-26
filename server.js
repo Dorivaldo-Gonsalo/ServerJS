969715cc69e46fe447fea66606c531c31b5e90ff
Dorivaldo-Gonsalo <dorivaldogoncaloferreira011@gmail.com>
2025-05-26 13:06:51 +0100

    Estrutura inicial do servidor

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = 14477;;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port,(error)=>{
+    if(error){
+        console.log('Erro ao conectar o servidor');;
+    }
+    console.log(`Servidor activado em http:localhost:${port}`);
+});
