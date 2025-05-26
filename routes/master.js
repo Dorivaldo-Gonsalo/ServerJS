4034017716204508730b21e3b2e81460e776ba42
Dorivaldo-Gonsalo <dorivaldogoncaloferreira011@gmail.com>
2025-05-26 14:07:52 +0100

    update da nav

--- routes/master.js
+++ routes/master.js
@@ -3,7 +3,7 @@ const router = Router();
 
 router.get('/', (req, res)=>{
     res.send(
-        "<h1 style='text-align:center;margin:10% 20%'> Bem-Vindo ao Sistema de gerenciamento da Relaxe.ao (SGR) </h1><br> <a href='/api/usuarios'><button>Usuários</button></a> <button>Contratos</button> <button>Residências</button> <button>Master</button>"
+        "<h1 style='text-align:center;margin:10% 20%'> Bem-Vindo ao Sistema de gerenciamento da Relaxe.ao (SGR) </h1><br> <a href='/api/usuarios'><button>Usuários</button></a> <a href='/api/contratos'><button>Contratos</button></a> <a href='/api/residencias'><button>Residências</button></a> "
     );
     
 });
